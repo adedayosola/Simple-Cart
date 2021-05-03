dca58b00038063a331c62e50d380ee67ebd25145
dayoadediran <gistwithdayo@yahoo.com>
2021-05-03 15:13:56 -0400

    first commit

---
+++ src/reducer.js
@@ -0,0 +1,54 @@
+const reducer = (state, action) => {
+  if (action.type === "CLEAR_CART") {
+    return { ...state, cart: [] };
+  }
+  if (action.type === "REMOVE_ITEM") {
+    let newCart = state.cart.filter((item) => item.id !== action.payload);
+    return { ...state, cart: newCart };
+  }
+  if (action.type === "INCREASE") {
+    let newCart = state.cart.map((item) => {
+      if (item.id === action.payload) {
+        return { ...item, amount: item.amount + 1 };
+      }
+      return item;
+    });
+    return { ...state, cart: newCart };
+  }
+  if (action.type === "DECREASE") {
+    let newCart = state.cart
+      .map((item) => {
+        if (item.id === action.payload) {
+          return { ...item, amount: item.amount - 1 };
+        }
+        return item;
+      })
+      .filter((item) => item.amount !== 0);
+    return { ...state, cart: newCart };
+  }
+  if (action.type === "GET_TOTALS") {
+    let { total, amount } = state.cart.reduce(
+      (cartTotal, item) => {
+        const { price, amount } = item;
+        cartTotal.amount += amount;
+        cartTotal.total += amount * price;
+        return cartTotal;
+      },
+      {
+        total: 0,
+        amount: 0,
+      }
+    );
+    total = parseFloat(total.toFixed(2));
+    return { ...state, total, amount };
+  }
+  if (action.type === "LOADING") {
+    return { ...state, loading: true };
+  }
+  if (action.type === "DISPLAY_ITEMS") {
+    return { ...state, cart: action.payload, loading: false };
+  }
+
+  return state;
+};
+export default reducer;
